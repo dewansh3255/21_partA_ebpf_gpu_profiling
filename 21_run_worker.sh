7651b24d3cbfe1312bbddaad8993c53f899dbf63
Sanskargoyal608 <sanskargoyal608@gmail.com>
2026-03-10 14:59:11 +0530

    Fixed master IP address for PyTorch DDP

--- 21_run_worker.sh
+++ 21_run_worker.sh
@@ -13,7 +13,7 @@ torchrun \
     --nnodes=2 \
     --node_rank=1 \
     --nproc_per_node=1 \
-    --master_addr="192.168.1.1" \
+    --master_addr="192.168.52.99" \
     --master_port=29500 \
     21_ml_workload.py --gpus 1 --epochs 5 --output "results/multi_node/21_worker_results.json"
 
